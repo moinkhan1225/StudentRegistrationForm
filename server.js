0f8626b5e78f0d85853fccb0dffda401edec39c9
Hakai <moinkhanrobin1@gmail.com>
2024-01-01 00:51:25 +0530

    Adding port

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 const express  = require("express");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/',((req,res)=>{
     res.sendFile(process.cwd() + "/index.html")
 }))
+
+app.listen(port,()=>{
+    console.log(`Listening from ${port}`)
+})
