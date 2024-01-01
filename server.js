a195c7c6977024b0c7a720706817186d02127db6
Hakai <moinkhanrobin1@gmail.com>
2024-01-01 14:39:48 +0530

    adding functionality to admin panel --FIX 26

--- server.js
+++ server.js
@@ -71,10 +71,9 @@ if(req.body.username=="admin" && req.body.password=="admin"){
 })
 app.get('/adminpanel',((req,res)=>{
     const searchName = req.query.search;
-    console.log(searchName);
 
     if (!searchName) {
-        return res.status(400).json({ error: 'Student Does not exist in our database' });
+        return res.json('Student Does not exist in our database' );
     }
 
     studentModel.find({student_name: searchName})
